49eee88aa53d53d22c73152460df5112369eca57
qhcjfdudn <jhjhyjin@naver.com>
2020-01-14 02:44:02 +0900

    bmi Calculator 클론 코딩

--- pjt/BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ pjt/BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,11 +2,55 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*;
+import android.content.Intent
+import android.preference.PreferenceManager
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+    private fun loadData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getInt("KEY_HEIGHT", 0)
+        val weight = pref.getInt("KEY_WEIGHT", 0)
+
+        if(height != 0 && weight != 0) {
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+
+        resultButton.setOnClickListener {
+
+            saveData(heightEditText.text.toString().toInt(),
+                weightEditText.text.toString().toInt())
+
+            /* 기존의 legacy 코드
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("weight", weightEditText.text.toString())
+            intent.putExtra("height", heightEditText.text.toString())
+            startActivity(intent)
+            */
+
+            // anko 라이브러리를 사용한 코드
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString(),
+                "height" to heightEditText.text.toString()
+            )
+        }
     }
 }
