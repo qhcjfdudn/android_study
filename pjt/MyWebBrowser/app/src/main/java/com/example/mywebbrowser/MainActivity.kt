357a021e2bdd07e961120c47eac9b6abddcecebd
qhcjfdudn <jhjhyjin@naver.com>
2020-01-17 06:47:45 +0900

    MyWebBrowser 클론코딩

--- pjt/MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ pjt/MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -1,12 +1,87 @@
 package com.example.mywebbrowser
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+        }
+
+        webView.loadUrl("https://www.google.com")
+
+        urlEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl("https://${urlEditText.text.toString()}")
+                toast(urlEditText.text.toString()).show()
+                true
+            }
+            else {
+                false
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("https://google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("https://naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("https://daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:031-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                // 문자 보내기
+                return true
+            }
+            R.id.action_email -> {
+                // 이메일 보내기
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
